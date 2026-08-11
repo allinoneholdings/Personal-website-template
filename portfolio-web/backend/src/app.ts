92346a4f9adce7e62318b6cad8a7ea2294240094
Isuru Rathnayaka <rathnayakaisuru422@gmail.com>
2026-08-11 10:00:58 +0530

    config:mongo db connected and express bug fixed

---
+++ portfolio-web/backend/src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Backend API is running"
+  });
+});
+
+export default app;
