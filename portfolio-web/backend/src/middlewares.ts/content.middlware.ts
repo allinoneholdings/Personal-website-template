ceccdc7cb2cc13566631ab7642c66d7c5db259aa
Isuru Rathnayaka <rathnayakaisuru422@gmail.com>
2026-08-11 10:43:10 +0530

    feat: add validator middlware to contents

---
+++ portfolio-web/backend/src/middlewares.ts/content.middlware.ts
@@ -0,0 +1,36 @@
+
+import { Request, Response, NextFunction } from "express";
+
+export const validateContent = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { title, name } = req.body;
+
+  // Required validation
+  if (!title || !name) {
+    return res.status(400).json({
+      message: "Title and name are required",
+    });
+  }
+
+  
+  const wordPattern = /^[A-Z][a-z]*$/;
+
+  if (!wordPattern.test(title)) {
+    return res.status(400).json({
+      message:
+        "Title must start with a capital letter and the remaining letters must be lowercase",
+    });
+  }
+
+  if (!wordPattern.test(name)) {
+    return res.status(400).json({
+      message:
+        "Name must start with a capital letter and the remaining letters must be lowercase",
+    });
+  }
+
+  next();
+};
